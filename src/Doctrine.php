653f0545a525d62f44703fed792a831ff0dae12b
ilyas <deckersilyas@gmail.com>
2025-02-22 23:13:12 +0100

    Set DB config

--- src/Doctrine.php
+++ src/Doctrine.php
@@ -2,8 +2,6 @@
 declare(strict_types=1);
 namespace Ody\DB;
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-
 class Doctrine
 {
     public static function boot($config)
--- src/Eloquent.php
+++ src/Eloquent.php
@@ -5,14 +5,23 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Eloquent
 {
-    public static function boot($config)
+    public static function boot($config): void
     {
         $capsule = new Capsule;
-        $capsule->addConnection($config);
+        $capsule->addConnection(
+            (new Eloquent())->setConfig($config)
+        );
         $capsule->setAsGlobal();
         $capsule->bootEloquent();
 
         // set timezone for timestamps etc
         date_default_timezone_set('UTC');
+    }
+
+    public function setConfig(array $config): array
+    {
+        $config['driver'] = $config['adapter'];
+        $config['database'] = $config['db_name'];
+        return $config;
     }
 }

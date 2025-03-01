16f313e80c0212d5d8f06c268b8a2d1ddeebd1d8
ilyas <deckersilyas@gmail.com>
2025-03-01 23:39:57 +0100

    Load commands in service provider

--- src/ServiceProviders/DatabaseServiceProvider.php
+++ src/ServiceProviders/DatabaseServiceProvider.php
@@ -3,9 +3,28 @@
 namespace Ody\DB\ServiceProviders;
 
 use Ody\Core\Foundation\Providers\ServiceProvider;
+use Ody\DB\Migrations\Command\CleanupCommand;
+use Ody\DB\Migrations\Command\CreateCommand;
+use Ody\DB\Migrations\Command\DiffCommand;
+use Ody\DB\Migrations\Command\DumpCommand;
+use Ody\DB\Migrations\Command\InitCommand;
+use Ody\DB\Migrations\Command\MigrateCommand;
+use Ody\DB\Migrations\Command\RollbackCommand;
+use Ody\DB\Migrations\Command\StatusCommand;
 
 class DatabaseServiceProvider extends ServiceProvider
 {
+    public array $commands = [
+        DiffCommand::class,
+        MigrateCommand::class,
+        StatusCommand::class,
+        CleanupCommand::class,
+        DumpCommand::class,
+        CreateCommand::class,
+        RollbackCommand::class,
+        InitCommand::class,
+    ];
+
     public function register()
     {
 
@@ -13,6 +32,19 @@ class DatabaseServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands = [
+                DiffCommand::class,
+                MigrateCommand::class,
+                StatusCommand::class,
+                CleanupCommand::class,
+                DumpCommand::class,
+                CreateCommand::class,
+                RollbackCommand::class,
+                InitCommand::class,
+            ];
+        }
+
         if (class_exists('Ody\DB\Eloquent')) {
             \Ody\DB\Eloquent::boot(
                 config('database.environments')[config('app.environment', 'local')]

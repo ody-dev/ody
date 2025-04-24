5095ac933d08cce05878f9a648d7fb1e460cb2df
ilyas <deckersilyas@gmail.com>
2025-04-24 19:55:53 +0200

    add example handlers

--- framework/routes/api.php
+++ framework/routes/api.php
@@ -7,7 +7,11 @@
  * Variables $router, $middleware, and $container are available from the RouteLoader.
  */
 
+use App\Handlers\CreateUserHandler;
+use App\Handlers\GetUserHandler;
 use App\Handlers\GetUsersHandler;
 use Ody\Foundation\Facades\Route;
 
 Route::get('/users', GetUsersHandler::class);
+Route::get('/users/{id}', GetUserHandler::class);
+Route::post('/users', CreateUserHandler::class);
